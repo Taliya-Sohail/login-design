a8dc32d987b465f7b14ca9735719d1976bd40251
Taliya Sohail <taliyasohail9821@gmail.com>
2024-01-28 21:43:54 +0500

    home bhi added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,16 +8,16 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  productList: any [] = [];
-  cartObj : any = {
+  productList: any[] = [];
+  cartObj: any = {
     "id": 5,
     "userId": 2,
-    products:[],
+    products: [],
     "Date": "2023-04-27T07:12:40.926Z"
   };
-  constructor(private productService: ProductService,private toastr: ToastrService) {
+  constructor(private productService: ProductService, private toastr: ToastrService) {
 
   }
   ngOnInit(): void {
@@ -25,24 +25,24 @@ export class HomeComponent implements OnInit{
   }
 
   loadAllProducts() {
-    this.productService.getAllProducts().subscribe((result: any)=>{
+    this.productService.getAllProducts().subscribe((result: any) => {
       this.productList = result;
       this.productService.productList = result
     })
   }
 
   addItemToCart(id: number) {
-    const product={productId:id,quantity:1}
+    const product = { productId: id, quantity: 1 }
     this.cartObj.products.push(product)
     console.log(this.cartObj)
-    this.productService.addToCart(this.cartObj).subscribe((result: any)=>{
-      if(result) {
-         this.productList = result;
-        // this.toastr.success("sucessfully added",'Success')
+    this.productService.addToCart(this.cartObj).subscribe((result: any) => {
+      if (result) {
+        this.productList = result;
+        this.toastr.success("sucessfully added", 'Success')
 
-        this.toastr.success('Login successful!', 'Success');
-                this.productService.cartAddedSubject.next(result.products);
-       }
+        //this.toastr.success('Login successful!', 'Success');
+        this.productService.cartAddedSubject.next(result.products);
+      }
     })
   }
 }
